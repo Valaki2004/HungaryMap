71830412b19786432c1bb3dbf1429d02fd76a96d
VTG <varastothgergo@ktch.hu>
2025-02-28 11:08:55 +0100

    comment

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -10,13 +10,18 @@ import { BaseService } from '../base.service';
 })
 export class CommentsComponent implements OnInit {
     comments : string[] = []
+    commentsString = {id:null,Helysegnev:'',Commnet:'',Email:''}
 
     constructor (private base:BaseService) {}
 
      ngOnInit( ) {
        this.base.getComments().subscribe((res)=>{
         this.comments = Object.values(res)  
-        
      })
     }
+     PostComment(){
+      this.base.createComment(this.comments)
+     }
 }
+
+
